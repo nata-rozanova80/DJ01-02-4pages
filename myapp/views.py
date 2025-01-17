5467fcb8aaf29191c2b4b253b94b0d464c654b99
NATALIA <nata.rozanova80@gmail.com>
2025-01-17 14:51:24 +0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -13,12 +13,5 @@ def test_view(request):
 def new(request):
     return render(request, 'myapp/new.html')
 
-# def home_view(request):
-#     return HttpResponse("<h1>Добро пожаловать на главную страницу!</h1>")
-#
-# def data_view(request):
-#     return HttpResponse("<h1>Это страница Data</h1><p>Содержимое для страницы Data.</p>")
-#
-# def test_view(request):
-#     return HttpResponse("<h1>Это страница Test</h1><p>Содержимое для страницы Test.</p>")
+
 
